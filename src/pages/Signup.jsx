86afb91efd175125762a66aeeb694edab64bea03
Gulshan <abhinavg90834@gmail.com>
2024-12-27 21:17:06 +0530

    signup fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -78,39 +78,6 @@ const Signup = () => {
             <div className='invalid-feedback'>{errors.userType.message}</div>
           )}
         </div>
-        <div className='mb-3'>
-          <label className='form-label d-block'>User Type</label>
-          <div className='form-check form-check-inline'>
-            <input
-              className='form-check-input'
-              type='radio'
-              id='supplier'
-              value='supplier'
-              {...register('userType')}
-              defaultChecked
-            />
-            <label className='form-check-label' htmlFor='supplier'>
-              Supplier
-            </label>
-          </div>
-          <div className='form-check form-check-inline'>
-            <input
-              className='form-check-input'
-              type='radio'
-              id='admin'
-              value='admin'
-              {...register('userType')}
-            />
-            <label className='form-check-label' htmlFor='admin'>
-              Admin
-            </label>
-          </div>
-          {errors.userType && (
-            <small className='text-danger d-block'>
-              {errors.userType.message}
-            </small>
-          )}
-        </div>
 
         <div className='mb-3'>
           <label>Email</label>
