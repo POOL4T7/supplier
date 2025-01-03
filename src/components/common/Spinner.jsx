ef432c9aee6f5cf504ed45b0a01d56983fdc7d49
pool4t9 <gulshan.gupta@omlogic.co.in>
2025-01-03 16:56:50 +0530

    button disable issue fixed

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -1,15 +1,22 @@
-const Spinner = () => {
+import PropTypes from "prop-types";
+
+const Spinner = ({ width = "100px", height = "100px" }) => {
   return (
-    <div className='container text-center mt-3'>
-      <div
-        className='spinner-border'
-        role='status'
-        style={{ width: '100px', height: '100px' }}
+    <span className="container text-center mt-3">
+      <span
+        className="spinner-border"
+        role="status"
+        style={{ width: width, height: height }}
       >
-        <span className='visually-hidden'>Loading...</span>
-      </div>
-    </div>
+        <span className="visually-hidden">Loading...</span>
+      </span>
+    </span>
   );
+};
+
+Spinner.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 export default Spinner;
